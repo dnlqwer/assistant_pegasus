6c1c1a3f7ff0dc79fab98cf00a415a49feb70b46
kriper <118538541+dnlqwer@users.noreply.github.com>
2023-06-08 08:43:11 +0500

    Add files via upload

---
+++ pegas/plugins/plugin_greetings.py
@@ -0,0 +1,27 @@
+# Приветствие (и демо-плагин)
+
+import random
+from vacore import VACore
+
+# функция на старте
+def start(core:VACore):
+    manifest = { # возвращаем настройки плагина - словарь
+        "name": "Привет", # имя
+        "version": "1.0", # версия
+        "require_online": False, # требует ли онлайн?
+
+        "commands": { # набор скиллов. Фразы скилла разделены | . Если найдены - вызывается функция
+            "привет|доброе утро": play_greetings,
+        }
+    }
+    return manifest
+
+def play_greetings(core:VACore, phrase: str): # в phrase находится остаток фразы после названия скилла,
+                                              # если юзер сказал больше
+                                              # в этом плагине не используется
+    # Проигрывание случайной приветственной речи
+    greetings = [
+        "И тебе привет, Даниил!",
+        "Рад тебя видеть, Даниил!",
+    ]
+    core.play_voice_assistant_speech(greetings[random.randint(0, len(greetings) - 1)])
